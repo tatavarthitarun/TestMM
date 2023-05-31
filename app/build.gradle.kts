1369a65d2f1ef44ec65aeeddd17202d6623a6249
tatavarthitarun <tatavarthitarun@gmail.com>
2023-05-31 13:36:38 +0530

    Initialize renovate/minor-and-patch-changes

---
+++ app/build.gradle.kts
@@ -0,0 +1,82 @@
+@file:Suppress("UnstableApiUsage")
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.detekt)
+    alias(libs.plugins.hilt)
+    alias(libs.plugins.kotlin)
+    alias(libs.plugins.kotlin.kapt)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.ktlint)
+}
+
+android {
+    compileSdk = 33
+    namespace = "eu.krzdabrowski.starter"
+
+    defaultConfig {
+        applicationId = "eu.krzdabrowski.starter"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildFeatures {
+        buildConfig = true
+        compose = true
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android.txt"),
+                "proguard-rules.pro"
+            )
+            // TODO: for development purposes, remember to create a release signing config when releasing proper app
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    composeOptions {
+        kotlinCompilerExtensionVersion = libs.versions.compose.compiler.get()
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    packaging {
+        resources.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+    }
+}
+
+dependencies {
+    implementation(project(":core"))
+    implementation(project(":basic-feature"))
+
+    implementation(libs.hilt)
+    implementation(libs.navigation) // needed for Room
+    implementation(libs.room.ktx)
+    implementation(libs.timber)
+
+    kapt(libs.hilt.compiler)
+    ksp(libs.room.compiler)
+
+    coreLibraryDesugaring(libs.desugar)
+
+    detektPlugins(libs.detekt.compose.rules)
+}
+
+ksp {
+    arg("room.schemaLocation", "$projectDir/schemas")
+}
